b9bca18d6165133899477ce9098350e50a1f333b
ilysim <illymarie1@live.com>
2022-07-07 22:02:55 -0500

    write score component

--- src/Score.jsx
+++ src/Score.jsx
@@ -1,10 +1,10 @@
 const Score = (props) => {
   return (
     <>
-      <h4>Date: </h4>
-      <h4>Score: </h4>
+      <h4>Date: {props.score.date}</h4>
+      <h4>Score: {props.score.score}</h4>
     </>
   );
 }
- 
+
 export default Score;
--- src/Student.jsx
+++ src/Student.jsx
@@ -6,7 +6,9 @@ const Student = (props) => {
     <div>
         <h2>Name: {props.student.name}</h2>
         <h2>Bio: {props.student.bio}</h2>
-        <Score />
+        {props.student.scores.map((score, idx) => 
+          <Score key={idx} score={score}/>
+        )}
 
     </div>
     </>
